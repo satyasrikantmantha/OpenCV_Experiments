70dd95a9a6c01394754b07a739f2db203b650b45
Satya Srikant Mantha <satyasrikantmantha@live.in>
2020-03-18 16:48:18 +0530

    Basic Image manipulation and contours detection.

--- 01_OpenCVBasics/utils.py
+++ 01_OpenCVBasics/utils.py
@@ -1,3 +1,11 @@
 from matplotlib import pyplot as plt
 import cv2
 
+def image_stats(image):
+    return image.shape
+
+#For displaying in Jupyter Notebook we use matplotlib
+def display_image(image, title):
+    plt.imshow(image)
+    plt.title(title)
+    plt.show()
